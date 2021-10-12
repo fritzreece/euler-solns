6e696ee4eae4af20d96419a55cb8237e4d19d5db
Fritz Reece <fritzreece@gmail.com>
2021-10-11 23:40:36 -0500

    92 made a bit faster using smaller memo

--- Problem92/squarechain.py
+++ Problem92/squarechain.py
@@ -1,22 +1,29 @@
+def sq_sum(n):
+    return sum(int(c) * int(c) for c in str(n))
 
+# note that 7 * 9^2 = 567. Therefore, any sum of digit of squares of numbers below 10^7
+# will be at most 486, so if we memoize all the values up to that, we can solve bigger numbers in one step
+# without using too much time building up a memo of bigger values
+# improves runtime ~30%
+def memo_up_to(bound=568):
+    res = [0 for i in range(bound)]
+    res[0] = 1
+    res[88] = 89
+    for i in range(2, bound):
+        temp = i
+        while temp != 1 and temp != 89:
+            temp = sq_sum(temp)
+        res[i-1] = temp
+    return res
 
-MEMO = {}
-# returns the first entry visited twice on square chain starting at n. memoized
-def sqc_loop(n):
-    curr = n
-    seen = set()
-    while curr not in MEMO and curr not in seen:
-        seen.add(curr)
-        curr = sum(int(c) ** 2 for c in str(curr))
-    if curr in MEMO:
-        curr = MEMO[curr]
-    for s in seen:
-        MEMO[s] = curr
-    return curr
+def sqc_loop(n, memo):
+    return memo[sq_sum(n)-1]
 
 # not terribly fast...
 from time import time
 if __name__ == "__main__":
    start = time()
-   print("Answer: " + str(sum(1 for i in reversed(range(1,10 ** 7)) if sqc_loop(89) == sqc_loop(i))))
+   memo = memo_up_to()
+   print("Memo generated in: " + str(time() - start))
+   print("Answer: " + str(sum(1 for x in range(1, 10 ** 7) if sqc_loop(x, memo) == 89)))
    print("Time: " + str(time() - start))
